e9b7811f3cb58a46503cf2254a302de2cb4a575b
xiachun <1334465191@qq.com>
2019-07-19 12:42:37 +0800

    更新 error num value
    更新 error num value

--- douban_spider.py
+++ douban_spider.py
@@ -42,7 +42,7 @@ class DoubanSpider(object):
             if len(context_list) == 0:
                 break
             self.save_content_list(context_list)
-            num += 40
+            num += 400
 
 
 if __name__ == "__main__":
