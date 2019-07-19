dedc2b782de2a060a7dcd7c3fa7c19434f9b1768
xiachunlin <1334465191@qq.com>
2019-07-19 14:19:13 +0800

    update

--- douban_spider.py
+++ douban_spider.py
@@ -42,7 +42,7 @@ class DoubanSpider(object):
             if len(context_list) == 0:
                 break
             self.save_content_list(context_list)
-            num += 5
+            num += 20
 
 
 if __name__ == "__main__":
